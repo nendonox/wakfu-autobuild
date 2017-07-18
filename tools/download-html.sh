2f459e537e98e0efe494583e61ef332b823e5ae9
nendo <rosukaruklame@gmail.com>
2017-07-18 21:05:46 +0900

    レアリティの指定を正しく修正

--- tools/download-html.sh
+++ tools/download-html.sh
@@ -1,4 +1,4 @@
 cd `dirname $0`
 mkdir -p ../dest/html 2> /dev/null
-wget 'https://www.wakfu.com/en/mmorpg/encyclopedia/armors?LVL_MIN=200&LVL_MAX=200&RARITY=4&RARITY=5&RARITY=7&size=120' -O ../dest/html/armors.html
-wget 'https://www.wakfu.com/en/mmorpg/encyclopedia/weapons?LVL_MIN=200&LVL_MAX=200&RARITY=4&RARITY=5&RARITY=7' -O ../dest/html/weapons.html
+wget 'https://www.wakfu.com/en/mmorpg/encyclopedia/armors?LVL_MIN=200&LVL_MAX=200&RARITY[]=4&RARITY[]=5&RARITY[]=7&size=120' -O ../dest/html/armors.html
+wget 'https://www.wakfu.com/en/mmorpg/encyclopedia/weapons?LVL_MIN=200&LVL_MAX=200&RARITY[]=4&RARITY[]=5&RARITY[]=7' -O ../dest/html/weapons.html
