ca98b33fdeb746749ba0ecc68ef571b99d6de437
Nendo <rosukaruklame@gmail.com>
2018-04-04 23:21:46 +0900

    パッチ1.59に対応 (#6)
    * パッチ1.59に対応
    
    * プリセットの修正漏れ

--- client/scripts/config/app-const.js
+++ client/scripts/config/app-const.js
@@ -103,9 +103,9 @@ app.constant('AppConst', {
     'Melee Mastery',
     'Distance Mastery',
     'Berserk Mastery',
-    'Mastery to 3 random elements',
-    'Mastery to 2 random elements',
-    'Mastery to 1 random elements',
+    'Mastery in 3 random elements',
+    'Mastery in 2 random elements',
+    'Mastery in 1 random elements',
     'Mastery Fire',
     'Mastery Earth',
     'Mastery Air',
@@ -133,8 +133,8 @@ app.constant('AppConst', {
       statValues: JSON.parse('{"Health Points":0.3,"AP":280,"MP":180,"WP":"20","Range":120,"Control":0,' +
         '"Block":4,"Critical Hit":13,"Critical Mastery":0.8,"Rear Mastery":0,"Healing Mastery":0,' +
         '"Area Mastery":"0.4","Single Target Mastery":"0.4","Melee Mastery":0,"Distance Mastery":1,' +
-        '"Berserk Mastery":0,"Mastery to 3 random elements":1,"Mastery to 2 random elements":0.8,' +
-        '"Mastery to 1 random elements":0,"Mastery Fire":0.25,"Mastery Earth":0.25,"Mastery Air":0.25,' +
+        '"Berserk Mastery":0,"Mastery in 3 random elements":1,"Mastery in 2 random elements":0.8,' +
+        '"Mastery in 1 random elements":0,"Mastery Fire":0.25,"Mastery Earth":0.25,"Mastery Air":0.25,' +
         '"Mastery Water":0.25,"90% Vivacity":0,"Elemental Resistance":4,"Resistance to 3 random elements":3,' +
         '"Resistance to 2 random elements":2,"Resistance to 1 random elements":1,"Resistance Fire":1,' +
         '"Resistance Water":1,"Resistance Air":1,"Resistance Earth":1,"Critical Resistance":"0.25",' +
@@ -147,8 +147,8 @@ app.constant('AppConst', {
       statValues: JSON.parse('{"Health Points":0.3,"AP":280,"MP":180,"WP":"20","Range":"100","Control":0,' +
         '"Block":4,"Critical Hit":"0","Critical Mastery":"0","Rear Mastery":0,"Healing Mastery":0,' +
         '"Area Mastery":"0.4","Single Target Mastery":"0.4","Melee Mastery":0,"Distance Mastery":1,' +
-        '"Berserk Mastery":0,"Mastery to 3 random elements":1,"Mastery to 2 random elements":0.8,' +
-        '"Mastery to 1 random elements":0,"Mastery Fire":0.25,"Mastery Earth":0.25,"Mastery Air":0.25,' +
+        '"Berserk Mastery":0,"Mastery in 3 random elements":1,"Mastery in 2 random elements":0.8,' +
+        '"Mastery in 1 random elements":0,"Mastery Fire":0.25,"Mastery Earth":0.25,"Mastery Air":0.25,' +
         '"Mastery Water":0.25,"90% Vivacity":0,"Elemental Resistance":4,"Resistance to 3 random elements":3,' +
         '"Resistance to 2 random elements":2,"Resistance to 1 random elements":1,"Resistance Fire":1,' +
         '"Resistance Water":1,"Resistance Air":1,"Resistance Earth":1,"Critical Resistance":"0.25",' +
@@ -161,7 +161,7 @@ app.constant('AppConst', {
       statValues: JSON.parse('{"Health Points":0.3,"AP":280,"MP":180,"WP":"50","Range":"120","Control":"75",' +
         '"Block":4,"Critical Hit":"0","Critical Mastery":"0","Rear Mastery":0,"Healing Mastery":0,"Area Mastery":"0",' +
         '"Single Target Mastery":"0","Melee Mastery":"0.8","Distance Mastery":"0","Berserk Mastery":0,' +
-        '"Mastery to 3 random elements":"0.8","Mastery to 2 random elements":0.8,"Mastery to 1 random elements":"0.8",' +
+        '"Mastery in 3 random elements":"0.8","Mastery in 2 random elements":0.8,"Mastery in 1 random elements":"0.8",' +
         '"Mastery Fire":0.25,"Mastery Earth":0.25,"Mastery Air":0.25,"Mastery Water":0.25,"90% Vivacity":0,' +
         '"Elemental Resistance":4,"Resistance to 3 random elements":3,"Resistance to 2 random elements":2,' +
         '"Resistance to 1 random elements":1,"Resistance Fire":1,"Resistance Water":1,"Resistance Air":1,' +
@@ -175,7 +175,7 @@ app.constant('AppConst', {
       statValues: JSON.parse('{"Health Points":0.3,"AP":280,"MP":180,"WP":"0","Range":120,' +
         '"Control":0,"Block":4,"Critical Hit":"13","Critical Mastery":0.8,"Rear Mastery":0,"Healing Mastery":0,' +
         '"Area Mastery":"0","Single Target Mastery":"1","Melee Mastery":0,"Distance Mastery":1,"Berserk Mastery":0,' +
-        '"Mastery to 3 random elements":1,"Mastery to 2 random elements":"1","Mastery to 1 random elements":"1",' +
+        '"Mastery in 3 random elements":1,"Mastery in 2 random elements":"1","Mastery in 1 random elements":"1",' +
         '"Mastery Fire":0.25,"Mastery Earth":0.25,"Mastery Air":0.25,"Mastery Water":0.25,"90% Vivacity":0,' +
         '"Elemental Resistance":4,"Resistance to 3 random elements":3,"Resistance to 2 random elements":2,' +
         '"Resistance to 1 random elements":1,"Resistance Fire":1,"Resistance Water":1,"Resistance Air":1,' +
